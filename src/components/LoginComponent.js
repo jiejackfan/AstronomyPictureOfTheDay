13215462566eb1bb32460c327ee8fda60db564f9
Joshi <joshi.mai>
2020-06-15 16:11:04 -0400

    added  login,profile,register

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import {login} from "../services/UserService";
+import {Link} from "react-router-dom";
 
-const LoginComponent = () =>
-    <div>
-        <h1>Login</h1>
-    </div>
+export default class Login extends React.Component {
 
-export default LoginComponent
+ state = {
+        username: '',
+        password: ''
+    }
+
+    login = (user) =>
+        login(user)
+            .then(currentUser => this.props.history.push('/profile'))
+render() {
+        return (
+<form>
+
+<h3> Login </h3>
+ <div className="form-group">
+        <label>Username</label>
+        <input    placeholder="Enter User Name"
+                  onChange={(e) => this.setState({username: e.target.value})}
+                  className="form-control"/>
+
+                </div>
+ <div className="form-group">
+                    <label>Password</label>
+                    <input   type="password"
+                    placeholder="Enter password"
+                              onChange={(e) => this.setState({password: e.target.value})}
+                              className="form-control"/>
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block"
+                             onClick={() => this.login(this.state)}>
+                 Sign in</button>
+                <p className="forgot-password text-right">
+                      <Link to="/register">Sign up</Link>
+                </p>
+            </form>
+);
+}
+}
--- src/components/RegisterComponent.js
+++ src/components/RegisterComponent.js
@@ -1,8 +1,61 @@
 import React from 'react';
+import {register} from "../services/UserService";
+import {Link} from "react-router-dom";
+export default class Register extends React.Component {
+    state = {
+        username: '',
+        password: '',
+        verifyPassword: ''
+    }
+ register = (user) =>
+         register(user)
+             .then(newUser => this.props.history.push('/profile'))
 
-const RegisterComponent = () =>
-    <div>
-        <h1>Register</h1>
-    </div>
 
-export default RegisterComponent
+                render() {
+                    return(
+                    <form>
+                    <h3> Register </h3>
+                       <div className="form-group">
+                              <label>User Name</label>
+
+                              <input value={this.state.username}
+                                         onChange={(e) => this.setState({
+                                          username: e.target.value
+                                                  })}
+                                                  className="form-control"
+                                                  placeholder="username"/>
+                                    </div>
+                             <div className="form-group">
+                                      <label>Password</label>
+                                      <input  value={this.state.password}
+                                         onChange={(e) => this.setState({
+                                           password: e.target.value
+                                                     })}
+                                                className="form-control"
+                                                type="password"
+                                      placeholder="Enter password"/>
+                                              </div>
+ <div className="form-group">
+                           <label>Verfiy Password</label>
+                            <input
+                                               value={this.state.verifyPassword}
+                                               onChange={(e) => this.setState({
+                                                   password: e.target.value
+                                               })}
+                                               className="form-control"
+                                               type="password"
+                                               placeholder="Enter password"/>
+        </div>
+
+  <button type="submit" className="btn btn-primary btn-block"
+                  onClick={() => this.register(this.state)}
+                     >
+                  Register </button>
+                 <p className="forgot-password text-right">
+                             <Link to="/login">Sign in?</Link>
+                 </p>
+</form>
+                      );
+                      }
+                      }
