3bf78de868e1e5116d9ae61bbf8230ebc5401827
Joshi <joshi.mai>
2020-06-18 11:53:19 -0400

    minor changes

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBarComponent from "./NavBarComponent";
-
+import Background from '../resources/pic.jpg';
 
 class SearchComponent extends React.Component {
 
@@ -21,26 +21,27 @@ class SearchComponent extends React.Component {
 
     render() {
         return(
-            <div>
+            <div >
                 <NavBarComponent/>
                 <form>
-                    <h5>Display Astronomy Picture of a Selected Date</h5>
-                    <label>
+                    <h5 className="d-flex justify-content-center">Display Astronomy Picture of a Selected Date</h5>
+                    <label className="d-flex justify-content-center">
                         Search by a single date: 
                         <input type="date"
                                placeholder="10-21-2015"
                                name="singleDate"
                                value={this.state.singleDate}
                                onChange={this.handleChange}/>
-                    </label>
-                    <button className="btn btn-danger"
+
+                    <button className="btn btn-danger float-right"
                             onClick={this.submitSingleDate}>
                                 Search
                     </button>
+                     </label>
                 </form>
                 <form>
-                    <h5>Display Astronomy Picture of the Day for a Range of Dates</h5>
-                    <label>
+                    <h5 className="d-flex justify-content-center">Display Astronomy Picture of the Day for a Range of Dates</h5>
+                    <label className="d-flex justify-content-center">
                         Start date:
                         <input type="date"
                                placeholder="01-01-2020"
@@ -48,7 +49,7 @@ class SearchComponent extends React.Component {
                                value={this.state.startDate}
                                onChange={this.handleChange}/>
                     </label>
-                    <label>
+                    <label className="d-flex justify-content-center">
                         End date:
                         <input type="date"
                                placeholder="02-01-2020"
@@ -56,10 +57,18 @@ class SearchComponent extends React.Component {
                                value={this.state.endDate}
                                onChange={this.handleChange}/>
                     </label>
-                    <button className="btn btn-danger"
+                    <div
+                            style={{
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center"
+                            }}
+                          >
+                    <button style={{justifyContent: 'center'}} className="btn btn-danger"
                             onClick={this.submitDateRange}>
                                 Search
                     </button>
+                    </div>
                 </form>
             </div>
         )
